be09f03b19c7b99d25a30bd269f228ecc36dcf2e
Jopanzo <jpnzo2k12@gmail.com>
2019-09-11 00:29:07 -0400

    Add files via upload

---
+++ src/fccApp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import fccApp
+admin.site.register(fccApp)
+# Register your models here.
---
+++ src/fccApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FccappConfig(AppConfig):
+    name = 'fccApp'
