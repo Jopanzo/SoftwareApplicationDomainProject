d3d212792f7e018e90c98904e4a9a204dfa0fe5e
Jopanzo <jpnzo2k12@gmail.com>
2019-09-14 13:31:21 -0400

    Delete models.py

--- src/fccApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class fccApp(models.Model):
-	title=models.CharField(max_length=120)
-	description=models.TextField(blank = True, null = True)
-	price= models.DecimalField(decimal_places=2, max_digits=1000)
-	time = models.DateTimeField()
-	summary= models.TextField(default='this is cool!')
